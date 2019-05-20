0bbd66d09fff279542c2874ef0762a1be018da5f
AlbertoPresta <apresta@login2.(none)>
2019-05-20 11:20:14 +0200

    lc

---
+++ Parallel_computing/gpu_ex/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd /home/apresta/HPC_exercise/Parallel_computing/gpu_ex
+module load cudatoolkit/10.0
+
+
+printf "\n\n\n" >> results.txt
+echo "512 thread_per_blocks" >> results.txt
+./512_shared_transpose >> results.txt
+
+printf "\n\n\n" >> results.txt
+echo "1024 thread_per_blocks" >> results.txt
+./1024_shared_transpose >> results.txt
+
+
+printf "\n\n\n" >> results.txt
+echo "64 thread_per_blocks" >> results.txt
+./64_shared_transpose >> results.txt
+
+
+printf "\n\n\n" >> results.txt
+echo "64 thread_per_blocks" >> results.txt
+./64_shared_transpose >> results.txt
+
+printf "\n\n\n" >> results.txt
+echo " naive" >> results.txt
+./transpose >> results.txt
+
+rm test.sh.*
+exit
